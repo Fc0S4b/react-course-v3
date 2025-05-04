0cbe9962ffb1c809f6f288e24e414cd225f70142
fc0S4b <87043410+Fc0S4b@users.noreply.github.com>
2025-05-03 23:36:48 -0400

    Create Element Function

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// function Greeting() {
+//   return <h2>My First Component</h2>
+// }
+
+// function Greeting() {
+//   return React.createElement('h2', {}, 'hello world')
+// }
 function Greeting() {
-  return <h2>My First Component</h2>
+  return (
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
