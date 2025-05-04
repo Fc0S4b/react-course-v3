fea74b0211b374249c71bbd1650cc594f3080f0d
fc0S4b <87043410+Fc0S4b@users.noreply.github.com>
2025-05-04 15:41:03 -0400

    props basic setup

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,26 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const author = 'Name'
+const title = 'Title of the book'
+const img = './images/book-1.jpg'
+
 const BookList = () => {
   return (
-    <section>
-      <Book />
-      <Book />
+    <section className="bookList">
+      <Book job="developer" />
+      <Book title="random title" number={22} />
       <Book />
     </section>
   )
 }
 
-const author = 'Name'
-
-const Book = () => {
-  const title = 'Title of the book'
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="asd.jpg" alt="book1" />
+      <img src={img} alt="book1" />
       <h2>{title}</h2>
-      <h2>{author.toUpperCase()}</h2>
-      {/* <p>{let x = 6}</p> error, solo expresiones */}
+      <h2>{author}</h2>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   )
 }
