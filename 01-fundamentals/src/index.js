eb24953b6d1eeffed4801f8b475918d835788998
fc0S4b <87043410+Fc0S4b@users.noreply.github.com>
2025-05-03 23:46:25 -0400

    jsx rules

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,17 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 // function Greeting() {
-//   return <h2>My First Component</h2>
+//   return (
+//     <>
+//       <div>
+//         <h3>hello people</h3>
+//         <ul>
+//           <li>
+//             <a href="#">Hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//     </>
+//   )
 // }
 
-// function Greeting() {
-//   return React.createElement('h2', {}, 'hello world')
-// }
 function Greeting() {
   return (
-    <div>
-      <h2>My First Component</h2>
-    </div>
+    <React.Fragment>
+      <div className="someValue">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">Hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input />
+    </React.Fragment>
   )
 }
 
